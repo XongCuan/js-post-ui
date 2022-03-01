570273eee9531429d69f52d9d21364addcd9d670
XuanCong <nguyenxuancongme@gmail.com>
2022-03-01 15:25:34 +0700

    Add axios

---
+++ js/api/axiosClient.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+// tao ra instance
+const axiosClient = axios.create({
+  baseURL: "http://js-post-api.herokuapp.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+localStorage.setItem("access_token", "fake - token");
+// Interceptors : apply 1 cai gi do vao request or response
+axiosClient.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+
+    // Attach token to request if exists
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+axiosClient.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger response
+    // Do something with response data
+
+    return response.data;
+  },
+  function (error) {
+    
+    console.log("axiosClient - response error", error.response);
+    if (!error.response) {
+      throw new Error("Network error. Please try again later");
+    }
+
+    if (error.response.status === 401) {
+      window.location.assign("/login.html");
+      return;
+    }
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('Hello cong dep trai');
+import postApi from "./api/postApi";
+import studentApi from "./api/studentApi";
+
+console.log("Hello cong dep trai");
+
+async function main() {
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    };
+    // const response = await postApi.getAll(queryParams);
+    const data = await postApi.getAll(queryParams);
+    console.log("main.js data", data);
+  } catch (error) {
+    console.log("Failed get all", error);
+  }
+  // const response = await axiosClient.get("/posts");
+}
+
+main();
